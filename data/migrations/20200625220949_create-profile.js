b3bb28f21a5008b106343eca00bfd76789655e3d
Steven Waldron <stevenwaldron@stevens-mbp.attlocal.net>
2021-03-21 09:48:14 -0700

    created favorites table schema

--- data/migrations/20200625220949_create-profile.js
+++ data/migrations/20200625220949_create-profile.js
@@ -31,9 +31,29 @@ exports.up = (knex) => {
       .onUpdate('CASCADE')
       .onDelete('CASCADE');     
     })
+    .createTable('favorites',table => {
+      table.increments();
+      table.string('city');
+      table.string('state');
+      table.float('diversity_index');
+      table.float('population');
+      table.float('rental_price');
+      table.string('crime');
+      table.string('air_quality_index');
+      table.float('walkability');
+      table.float('livability');
+      table.float('latitude');
+      table.float('longitude');
+      table.string('profile_id')
+      .unsigned()
+      .notNullable()
+      .references('id')
+      .inTable('profiles')
+    })
+    
 };
 
 exports.down = (knex) => {
   return knex.schema
-  .dropTableIfExists('cities').dropTableIfExists('profiles');
+  .dropTableIfExists('cities').dropTableIfExists('profiles').dropTableIfExists('favorites');
 };
