69873cc7b7a91462bc9914ff500211f6ac696da4
Carlos <c.baez1223@yahoo.com>
2021-03-30 22:32:51 -0400

    updated favorites migrations

--- data/migrations/20210325221908_fav_table.js
+++ data/migrations/20210325221908_fav_table.js
@@ -1,23 +1,13 @@
 exports.up = function (knex) {
   return knex.schema.createTable('favorites', function (table) {
     table.increments();
-    table.string('city');
-    table.string('state');
-    table.float('diversity_index');
-    table.float('population');
-    table.float('rental_price');
-    table.string('crime');
-    table.string('air_quality_index');
-    table.float('walkability');
-    table.float('livability');
-    table.float('latitude');
-    table.float('longitude');
     table
       .string('profile_id')
       .unsigned()
       .notNullable()
       .references('id')
       .inTable('profiles');
+    table.string('city_id').notNullable().references('id').inTable('cities');
   });
 };
 
