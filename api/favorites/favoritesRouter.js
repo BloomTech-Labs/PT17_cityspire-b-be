4120647b77efe57ed1a57514ab602e50ba135177
Steven Waldron <stevenwaldron@stevens-mbp.attlocal.net>
2021-04-20 19:38:16 -0700

    added functionality to prevend duplicate saves

--- api/favorites/favoritesRouter.js
+++ api/favorites/favoritesRouter.js
@@ -107,11 +107,3 @@ router.delete('/:profileid/favorites/:cityid', (req, res) => {
 });
 
 module.exports = router;
-
-// const pfavs = await db('favorites').where({profile_id: userId})
-//   for(let i in pfavs){
-//     const c = await db('cities').where({id:i.city_id})
-//     if(req.body.city == c[0].city){
-//       res.status(500).json({message:'already in favs!'})
-//     }
-//   }
